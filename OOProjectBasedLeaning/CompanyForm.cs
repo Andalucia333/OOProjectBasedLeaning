de197e656d810191521a53c00330a0cffdbaf52f
iidayuuta <C3011@oic.osaka.jp>
2025-06-27 22:43:44 +0900

    各FromをDragDropFromに変更

--- OOProjectBasedLeaning/CompanyForm.cs
+++ OOProjectBasedLeaning/CompanyForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace OOProjectBasedLeaning
 {
 
-    public partial class CompanyForm : Form
+    public partial class CompanyForm : DragDropForm
     {
 
         private Company company = NullCompany.Instance;
