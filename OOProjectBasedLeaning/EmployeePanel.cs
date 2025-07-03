3dce09bf889d7276a3e0400f429783126df65862
iidayuuta <C3011@oic.osaka.jp>
2025-07-03 14:59:30 +0900

    EmployeeStatusLabel作成

--- OOProjectBasedLeaning/EmployeePanel.cs
+++ OOProjectBasedLeaning/EmployeePanel.cs
@@ -63,4 +63,32 @@ namespace OOProjectBasedLeaning
 
     }
 
+    public class EmployeeStatusLabel : Label, Observer
+    {
+        private Employee employee = NullEmployee.Instance;
+
+        public EmployeeStatusLabel()
+        {
+            InitializeComponent();
+        }
+
+        public void InitializeComponent()
+        {
+            this.AutoSize = true;
+            this.Font = new Font("Arial",10,FontStyle.Regular);
+
+            Update(this);
+        }
+
+        public void Update(object sender)
+        {
+            if (employee.IsAtWork())
+            {
+                Text = "勤務中";
+                ForeColor = Color.Red;
+            }
+            //else if(employee.)
+        }
+    }
+
 }
